ce4c5b16f88fba18a67429bd71f2f49712d28837
younes ait m'ha <younesaitmha@gmail.com>
2022-02-21 23:56:00 +0100

    fix pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,24 @@
+
+import { ApolloProvider } from '@apollo/client';
 import '../styles/globals.css'
 import '../styles/dashboard.css'
 import '../styles/modal.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import { useApollo } from '../lib/ApolloClientt'
 
 import React from 'react';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const apolloClient = useApollo(pageProps)
+  const getLayout = Component.getLayout || ((page) => page)
+
+  return (
+    <>
+      <ApolloProvider client={apolloClient}>
+        {getLayout(<Component {...pageProps} />)}
+      </ApolloProvider>
+    </>
+  )
 }
 
 export default MyApp
